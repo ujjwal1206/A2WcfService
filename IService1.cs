fb70b2056781cc13e773c99fd80604be3397ace9
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2025-02-27 20:19:24 +0530

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace A2WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate= "/secretnumber?x={x}&y={y}", ResponseFormat =WebMessageFormat.Json)]
+        int SecretNumber(int x, int y);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/checknumber?x={x}&y={y}", ResponseFormat = WebMessageFormat.Json)]
+        string CheckNumber(int x, int y);
+    }
+
+
+   
+}
